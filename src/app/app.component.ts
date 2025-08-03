e373a41d61f9cbdf2ec0011dfb4dc980e0e7af51
miguel8888888 <86738300+miguel8888888@users.noreply.github.com>
2025-08-03 12:31:02 -0500

    solo consulta de base de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,11 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {HomeComponent} from './home/home.component';
-import {RouterModule} from '@angular/router';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {CurrencyPipe, CommonModule} from '@angular/common';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatTreeModule} from '@angular/material/tree';
-import SidenavComponent  from './components/sidenav/sidenav.component';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RegistrosService } from './services/registros.service';
 
-// interface FoodNode {
-//   name: string;
-//   children?: FoodNode[];
-// }
+
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, SidenavComponent, MatSlideToggleModule, MatIconModule, MatDividerModule, MatButtonModule, MatCardModule, MatGridListModule, CurrencyPipe, MatSidenavModule, MatTreeModule, MatExpansionModule, HomeComponent, CommonModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
